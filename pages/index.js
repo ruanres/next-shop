b9fc249d26e224515ca73ee3418973fdbc3d1fe0
Ruan Eloy <ruanr18@gmail.com>
2023-04-06 14:14:30 -0300

    feat: fetch products

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
+import {getProducts} from "@/lib/products";
 import Head from "next/head";
+import Link from "next/link";
 
-export default function Home() {
+export async function getStaticProps() {
+	console.log("[getStaticProps]");
+	const products = await getProducts();
+	return {
+		props: {
+			products
+		},
+		revalidate: 5 * 60
+	};
+}
+
+export default function Home({products}) {
 	return (
 		<>
 			<Head>
 				<title>Create Next App</title>
 			</Head>
 			<main className="p-5">
-				<h1 className="font-bold underline">Next Shop</h1>
+				<h1 className="font-bold">Next Shop</h1>
+				<ul>
+					{products.map(product => (
+						<li key={product.id}>
+							<Link href={`products/${product.id}`}>
+								{product.title}
+							</Link>
+						</li>
+					))}
+				</ul>
 			</main>
 		</>
 	);
