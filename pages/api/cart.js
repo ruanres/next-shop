5b77e36c878ca416ab3790954cb13186ed4f7dfa
Ruan Eloy <ruanr18@gmail.com>
2023-04-25 14:50:00 -0300

    feat: create handler to add product to cart

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -17,7 +17,7 @@ function stripCartItem(item) {
 }
 
 
-async function handleCart(req, res) {
+async function handleGetCart(req, res) {
 	const { jwt } = req.cookies;
 	if (!jwt) {
 		res.status(401).end();
@@ -35,6 +35,40 @@ async function handleCart(req, res) {
 		});
 	} catch (error) {
 		res.status(401).end();
+	}
+}
+
+async function handlePostCart(req, res) {
+	const { jwt } = req.cookies;
+	if (!jwt) {
+		res.status(401).end();
+		return;
+	} 
+	const { productId, quantity } = req.body;
+	try {
+		await fetchJson(`${CMS_URL}/cart-items`, {
+			method: 'POST',
+			headers: {
+				'Authorization': `Bearer ${jwt}`,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ product: productId, quantity })
+		});
+
+		res.status(200).json({});
+	} catch (error) {
+		res.status(401).end();
+	}
+}
+
+async function handleCart(req, res) {
+	switch(req.method) {
+	case 'GET':
+		return handleGetCart(req, res);
+	case 'POST':
+		return handlePostCart(req, res);
+	default:
+		res.status(405).end();
 	}
 }
 
